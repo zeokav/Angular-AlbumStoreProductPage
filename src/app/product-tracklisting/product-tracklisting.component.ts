78ffa4b6b6429bcbf932c445be8d630dadc0eb11
Saket Singh <sakets@vmware.com>
2018-07-12 23:37:29 +0530

    tracklisting fix

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -15,7 +15,7 @@ export class ProductTracklistingComponent implements OnInit {
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe((response) =>
+    this._productService.getAlbum(1).subscribe(response =>
     this.albumInfo = response);
   }
 
