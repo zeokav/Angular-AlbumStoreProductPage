35c906ea37b48f7fb82c010e665b7d4cd208de70
Saket Singh <sakets@vmware.com>
2018-07-12 23:14:50 +0530

    interface

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { ProductService } from '../product.service';
 
+import { Album } from '../album';
+
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
@@ -9,12 +11,14 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  albumInfo;
+  albumInfo: Album;
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
     this._productService.getAlbum(1).subscribe(response =>
     this.albumInfo = response);
   }
+
+
 
 }
